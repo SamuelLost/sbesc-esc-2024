a7aab46388006c343f9642ffe5ab2b965ce16d23
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2024-08-26 17:37:18 -0300

    feat: add interfaces for UART and LoRa module
    1) Implement UART driver to send AT commands to the LoRa module.
    2) Create lora_module component to manage LoRa communication.
    
    Signed-off-by: Samuel Henrique <samuelhenriqec@alu.ufc.br>

--- firmware/components/lora_module/include/lora_module.h
+++ firmware/components/lora_module/include/lora_module.h
@@ -1,1 +1,56 @@
-void func(void);
+#ifndef _LORA_MODULE_H_
+#define _LORA_MODULE_H_
+
+#include "uart_driver.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct lora_module_t {
+    uart_t uart;
+} lora_module_t;
+
+/**
+ * @brief Initialize the LoRa module
+ * 
+ * @param lora LoRa module configuration structure
+ * @return true
+ * @return false 
+ */
+bool lora_module_init(lora_module_t *lora);
+
+/**
+ * @brief Deinitialize the LoRa module
+ * 
+ * @param lora LoRa module configuration structure
+ */
+void lora_module_deinit(lora_module_t *lora);
+
+/**
+ * @brief Send data to the LoRa module
+ * 
+ * @param lora LoRa module configuration structure
+ * @param data Data buffer
+ * @param size Data size
+ * @return true
+ * @return false 
+ */
+bool lora_module_send(lora_module_t *lora, uint8_t *data, uint32_t size);
+
+/**
+ * @brief Receive data from the LoRa module
+ * 
+ * @param lora LoRa module configuration structure
+ * @param data Data buffer
+ * @param size Data size
+ * @return true
+ * @return false 
+ */
+bool lora_module_receive(lora_module_t *lora, uint8_t *data, uint32_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _LORA_MODULE_H_
--- firmware/components/lora_module/lora_module.c
+++ firmware/components/lora_module/lora_module.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
 #include "lora_module.h"
 
-void func(void)
-{
+bool lora_module_init(lora_module_t *lora_module) {
+    return true;
+}
 
+void lora_module_deinit(lora_module_t *lora_module) {}
+
+bool lora_module_send(lora_module_t *lora_module, uint8_t *data, uint32_t size) {
+    return true;
+}
+
+bool lora_module_receive(lora_module_t *lora_module, uint8_t *data, uint32_t size) {
+    return true;
 }
