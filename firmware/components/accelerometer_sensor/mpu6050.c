110c62485252ea9a2b40a0e0b2a3847434aa5305
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2024-09-03 11:07:10 -0300

    feat: add more validation and guardrail
    1) Add macro RESTART
    2) Add validation in i2c_driver component
    
    Signed-off-by: Samuel Henrique <samuelhenriqec@alu.ufc.br>

--- firmware/components/accelerometer_sensor/mpu6050.c
+++ firmware/components/accelerometer_sensor/mpu6050.c
@@ -10,7 +10,14 @@ static inline void _convert_to_mps2(acceleration_data_t* accel_data, uint16_t sc
 static inline void _convert_to_g(acceleration_data_t* accel_data, uint16_t scale_factor);
 static inline uint16_t _mpu6050_get_lsb_sensitivity(accel_range_t accel_range);
 
+/******************************* PRIVATE ATTRIBUTES *******************************/
+static uint16_t _accel_sensitivity;
+
 bool mpu6050_init(mpu6050_t* config) {
+
+    if (!IS_VALID(config)) {
+        return false;
+    }
 
     if (!i2c_init(&config->i2c)) {
         return false;
@@ -32,11 +39,15 @@ bool mpu6050_init(mpu6050_t* config) {
         return false;
     }
 
+    _accel_sensitivity = _mpu6050_get_lsb_sensitivity(config->accel_range);
+
     return true;
 }
 
 void mpu6050_deinit(mpu6050_t* config) {
-    i2c_deinit(&config->i2c);
+    if (IS_VALID(config)) {
+        i2c_deinit(&config->i2c);
+    }
 }
 
 bool mpu6050_is_alive(mpu6050_t* config) {
@@ -44,7 +55,7 @@ bool mpu6050_is_alive(mpu6050_t* config) {
     if (!IS_VALID(config)) {
         return false;
     }
-    
+
     uint8_t data = i2c_read_byte(&config->i2c, config->addr, MPU6050_WHO_AM_I, TIMEOUT);
 
     return data == MPU6050_DEFAULT_ADDR;
@@ -69,13 +80,12 @@ bool mpu6050_get_acceleration(mpu6050_t* config, acceleration_data_t* accel_data
     accel_data->accel_z.raw = (buffer[4] << 8) | buffer[5];
 
     if (unit != ACCEL_RAW) {
-        uint16_t scale_factor = _mpu6050_get_lsb_sensitivity(config->accel_range);
 
         if (unit == ACCEL_MPS2) {
-            _convert_to_mps2(accel_data, scale_factor);
+            _convert_to_mps2(accel_data, _accel_sensitivity);
         }
         else if (unit == ACCEL_G) {
-            _convert_to_g(accel_data, scale_factor);
+            _convert_to_g(accel_data, _accel_sensitivity);
         }
     }
 
@@ -97,22 +107,22 @@ static inline uint16_t _mpu6050_get_lsb_sensitivity(accel_range_t accel_range) {
     }
 }
 
-static inline void _convert_to_mps2(acceleration_data_t* accel_data, uint16_t scale_factor) {
-    if (!IS_VALID(accel_data) || scale_factor == 0) {
+static inline void _convert_to_mps2(acceleration_data_t* accel_data, uint16_t accel_sensitivity) {
+    if (!IS_VALID(accel_data) || accel_sensitivity == 0) {
         return;
     }
 
-    accel_data->accel_x.converted = ((float) accel_data->accel_x.raw / scale_factor) * G_FORCE;
-    accel_data->accel_y.converted = ((float) accel_data->accel_y.raw / scale_factor) * G_FORCE;
-    accel_data->accel_z.converted = ((float) accel_data->accel_z.raw / scale_factor) * G_FORCE;
+    accel_data->accel_x.converted = ((float)accel_data->accel_x.raw / accel_sensitivity) * G_FORCE;
+    accel_data->accel_y.converted = ((float)accel_data->accel_y.raw / accel_sensitivity) * G_FORCE;
+    accel_data->accel_z.converted = ((float)accel_data->accel_z.raw / accel_sensitivity) * G_FORCE;
 }
 
-static inline void _convert_to_g(acceleration_data_t* accel_data, uint16_t scale_factor) {
-    if (!IS_VALID(accel_data) || scale_factor == 0) {
+static inline void _convert_to_g(acceleration_data_t* accel_data, uint16_t accel_sensitivity) {
+    if (!IS_VALID(accel_data) || accel_sensitivity == 0) {
         return;
     }
 
-    accel_data->accel_x.converted = (float) accel_data->accel_x.raw / scale_factor;
-    accel_data->accel_y.converted = (float) accel_data->accel_y.raw / scale_factor;
-    accel_data->accel_z.converted = (float) accel_data->accel_z.raw / scale_factor;
+    accel_data->accel_x.converted = (float)accel_data->accel_x.raw / accel_sensitivity;
+    accel_data->accel_y.converted = (float)accel_data->accel_y.raw / accel_sensitivity;
+    accel_data->accel_z.converted = (float)accel_data->accel_z.raw / accel_sensitivity;
 }
