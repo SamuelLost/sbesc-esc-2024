7b180ecf3af0f3a339d7b0a06778e693b7998751
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2024-10-08 15:51:45 -0300

    feat: add interfaces and enums to SHT30 driver
    Signed-off-by: Samuel Henrique <samuelhenriqec@alu.ufc.br>

--- firmware/components/temperature_sensor/include/sht30.h
+++ firmware/components/temperature_sensor/include/sht30.h
@@ -11,12 +11,37 @@
 extern "C" {
 #endif
 
+// SHT30 sensor modes
+typedef enum {
+    SHT30_SINGLE_SHOT = 0, // One single measurement
+    SHT30_PERIODIC_0_5_MPS, // Periodic measurement, 0.5 measurements per second
+    SHT30_PERIODIC_1_MPS,   // Periodic measurement, 1 measurement per second
+    SHT30_PERIODIC_2_MPS,   // Periodic measurement, 2 measurements per second
+    SHT30_PERIODIC_4_MPS,   // Periodic measurement, 4 measurements per second
+    SHT30_PERIODIC_10_MPS,  // Periodic measurement, 10 measurements per second
+} sht30_mode_t;
+
+// SHT30 repeatability modes
+typedef enum {
+    SHT30_HIGH = 0, // High repeatability
+    SHT30_MEDIUM,   // Medium repeatability
+    SHT30_LOW,      // Low repeatability
+} sht30_repeat_t;
+
+// SHT30 sensor data structure
 typedef struct {
     i2c_t i2c;
     uint8_t addr; // Device I2C address
-    float temperature; // (Internal) Temperature value
-    float humidity;   // (Internal) Humidity value
+    sht30_mode_t mode; // Sensor mode
+    sht30_repeat_t repeat; // Repeatability mode
 } sht30_t;
+
+// SHT30 data structure
+typedef struct {
+    float temperature; // Temperature value
+    float humidity;    // Humidity value
+} sht30_data_t;
+
 
 /**
  * @brief Initialize the SHT30 sensor
@@ -49,6 +74,15 @@ float sht30_get_temperature(sht30_t *sht30);
  * @return float 
  */
 float sht30_get_humidity(sht30_t *sht30);
+
+/**
+ * @brief Get the status of the sensor
+ * 
+ * @param sht30
+ * @return true
+ * @return false
+ */
+bool sht30_get_status(sht30_t *sht30);
 
 #ifdef __cplusplus
 }
--- firmware/components/temperature_sensor/sht30.c
+++ firmware/components/temperature_sensor/sht30.c
@@ -1,13 +1,59 @@
 #include <stdio.h>
 #include "sht30.h"
 
-typedef enum sht30_command_t {
-    SHT30_CMD_FETCH_DATA = 0xE000,
+#define TIMEOUT 50
+
+typedef enum {
+    SHT30_FETCH_DATA_CMD = 0xE000,
+    SHT30_RESET_CMD = 0x30A2,
+    STH30_STATUS_CMD = 0xF32D,
     // Other commands
 
 } sht30_command_t;
 
+// SHT30 measurement commands based on the sensor mode without clock stretching
+const uint16_t SHT30_MEASURE_CMD[6][3] = {
+    {0x2400,0x240b,0x2416},    // [SHT30_SINGLE_SHOT     ][HIGH, MEDIUM, LOW] 
+    {0x2032,0x2024,0x202f},    // [SHT30_PERIODIC_0_5_MPS][HIGH, MEDIUM, LOW]
+    {0x2130,0x2126,0x212d},    // [SHT30_PERIODIC_1_MPS  ][HIGH, MEDIUM, LOW]
+    {0x2236,0x2220,0x222b},    // [SHT30_PERIODIC_2_MPS  ][HIGH, MEDIUM, LOW]
+    {0x2234,0x2322,0x2329},    // [SHT30_PERIODIC_4_MPS  ][HIGH, MEDIUM, LOW]
+    {0x2737,0x2721,0x272a}     // [SHT30_PERIODIC_10_MPS ][HIGH, MEDIUM, LOW]
+};
+
+// SHT30 measurement duration (ms) based on the sensor mode
+const uint8_t SHT30_MEASURE_DURATION_MS[3] = {15, 6, 4};
+
+static bool _sht30_reset(sht30_t *sht30);
+static uint8_t _sht30_crc8(uint8_t *data, uint8_t len);
+static bool _sht30_read(sht30_t *sht30, uint8_t *data, uint32_t len);
+static bool _sht30_write(sht30_t *sht30, sht30_command_t sht30_cmd);
+
 bool sht30_init(sht30_t *sht30) {
+    if (!i2c_init(&sht30->i2c)) {
+        return false;
+    }
 
     return true;
+}
+
+void sht30_deinit(sht30_t *sht30) {
+    i2c_deinit(&sht30->i2c);
+}
+
+float sht30_get_temperature(sht30_t *sht30) {
+    uint8_t data[6] = {0};
+    uint16_t temperature = 0;
+
+    if (!i2c_write(&sht30->i2c, sht30->addr, SHT30_FETCH_DATA_CMD, 0, TIMEOUT)) {
+        return 0;
+    }
+
+    if (!i2c_read(&sht30->i2c, sht30->addr, SHT30_FETCH_DATA_CMD, data, 6, TIMEOUT)) {
+        return 0;
+    }
+
+    temperature = (data[0] << 8) | data[1];
+
+    return -45 + 175 * (temperature / 65535.0);
 }
